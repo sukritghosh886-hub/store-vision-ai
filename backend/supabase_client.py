e9973b92b57f48ee778a6593937368f72565b468
sukritghosh886-hub <sukritghosh886@gmail.com>
2026-09-05 21:16:38 +0530

    Update supabase_client.py

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -3,19 +3,12 @@ import os
 from dotenv import load_dotenv
 from supabase import Client, create_client
 
-
 load_dotenv()
 
 
 def create_supabase_client() -> Client:
-
-    url = os.getenv(
-        "SUPABASE_URL"
-    )
-
-    key = os.getenv(
-        "SUPABASE_PUBLISHABLE_KEY"
-    )
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_PUBLISHABLE_KEY")
 
     if not url:
         raise RuntimeError(
@@ -27,7 +20,4 @@ def create_supabase_client() -> Client:
             "SUPABASE_PUBLISHABLE_KEY is missing."
         )
 
-    return create_client(
-        url,
-        key,
-    )
+    return create_client(url, key)
